9205c62efa72d5bebd1f8e236e4aceda6c8acbec
anujdubey88 <anujd1595@gmail.com>
2024-05-12 11:38:30 +0530

    emailjs is added

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,50 +1,40 @@
-import React from "react";
+import React, { useState} from 'react';
+import emailjs from '@emailjs/browser';
 import "./contact.css";
-import { apiConnector } from "../../Services/apiConnectors";
 import { toast } from "react-hot-toast"
-import { endpoints } from "../../Services/apis";
-import { setLoading } from "../../slices/authSlice"
-import { useDispatch } from "react-redux";
 
-const {EMAIL_API}=endpoints;
 
 const Contact = () => {
-  const dispatch = useDispatch();
+
+  const [name,setName]=useState('');
+  const [email,setEmail]=useState('');
+  const [message,setMessage]=useState('');
+
+  const templateParams={
+    from_name:name,
+    from_email:email,
+    to_name:'SharpCode',
+    message:message
+  }
   const map =
     "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3393.393908365708!2d76.35820031451388!3d30.837560181461395!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x391aa6b83f6b0561%3A0x86391ff23c66b6e7!2sSant%20Longowal%20Institute%20of%20Engineering%20and%20Technology!5e0!3m2!1sen!2sin!4v1659802586404!5m2!1sen!2sin";
 
-  const handleFormSubmit = async(e) => {
-    e.preventDefault();
 
-    const formData = new FormData(e.target);
-    const name = formData.get("name");
-    const email = formData.get("email");
-    const subject = formData.get("subject");
-    const message = formData.get("message");
-    console.log(name,email,subject,message);
-
-    const toastId = toast.loading("Loading...")
-
-    dispatch(setLoading(true))
-    try {
-
-      const response = await apiConnector("POST",EMAIL_API, {
-        name,email,subject,message,
-        
-      })
-
-      console.log("response from signUp : ", response);
-      if (response.status!==200) {
-        throw new Error(response.data.message)
-      }
-      toast.success(response.data.message)
-      
-    } catch (error) {
-      toast.error("hii",error.message)
-    }
-    dispatch(setLoading(false))
-    toast.dismiss(toastId)
-  };
+    const sendEmail = (e) => {
+      e.preventDefault();
+      emailjs
+        .sendForm(process.env.SERVICE_ID,process.env.TEMPLATE_ID,templateParams,process.env.PUBLIC_KEY)
+        .then(
+          (response) => {
+            toast.success("Message is sent!!")
+            console.log('SUCCESS!');
+          },
+          (error) => {
+            toast.error("Please try again!!")
+            console.log('FAILED...', error.text);
+          },
+        );
+    };
 
   return (
     <>
@@ -65,11 +55,10 @@ const Contact = () => {
               </div>
             </div>
             <h3>Send your message</h3>
-            {/* <form onSubmit={handleFormSubmit}> */}
-            <form>
+            <form  onSubmit={sendEmail}>
               <div className="flexSB">
-                <input type="text" name="name" placeholder="Name" required />
-                <input type="email" name="email" placeholder="Email" required />
+                <input type="text" name="user_name" placeholder="Name" onChange={(e)=> setName(e.target.value)} required />
+                <input type="email" name="user_email" placeholder="Email" onChange={(e)=> setEmail(e.target.value)} required />
               </div>
               <input type="text" name="subject" placeholder="Subject" required />
               <textarea
@@ -77,6 +66,7 @@ const Contact = () => {
                 cols="30"
                 rows="3"
                 placeholder="Create a message here..."
+                onChange={(e)=>setMessage(e.target.value)}
                 required
               ></textarea>
               <button type="submit"  className="primary-btn primbtn">
